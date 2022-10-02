21d329a62fb49c043c8e4cacd0c2d71a9c872808
tasduqali <tasduqali2@gmail.com>
2022-10-02 14:12:52 +0400

    fixed url

--- src/Connection/API/api.js
+++ src/Connection/API/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// let url = "https://finiksbackend.herokuapp.com";
-let url = "http://localhost:3001";
+let url = "https://finiksbackend.herokuapp.com";
+// let url = "http://localhost:3001";
 
 const instance = axios.create({
   baseURL: url,
